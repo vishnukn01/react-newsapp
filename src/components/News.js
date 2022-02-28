ec8e1723d0ea5fb9f6a2c0fd095743e0cf4c457c
Vishnu KN <vishnunagaraj7@gmail.com>
2022-02-28 19:47:37 +0530

    Added next btn pagination

--- src/components/News.js
+++ src/components/News.js
@@ -7,12 +7,21 @@ export class News extends Component {
     super()
     this.state = {
       articles: [],
-      loading: false
+      loading: false,
+      page:1
     }
   }
   
+  nextHandler = async() => {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=80ac5a3fa8f44c10ab514ff0d8fb51d3&page=${this.state.page+1}`;
+      let data = await fetch(url);
+      let parsedData = await data.json()
+      console.log(parsedData);
+      this.setState({articles: parsedData.articles, page: this.state.page + 1})
+  }
+
   async componentDidMount(){
-    let url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=80ac5a3fa8f44c10ab514ff0d8fb51d3';
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=80ac5a3fa8f44c10ab514ff0d8fb51d3&page=${this.state.page}`;
     let data = await fetch(url);
     let parsedData = await data.json()
     console.log(parsedData);
@@ -20,7 +29,6 @@ export class News extends Component {
   }
 
   render() {
-    console.log('hello from render')
     return (
       <div className="container my-3">
         <h2>Top Headlines from India</h2>
@@ -29,9 +37,10 @@ export class News extends Component {
               return <div key={article.url} className="col-md-4">
                       <NewsItem title={article.title} description={article.description} imageUrl={article.urlToImage}  newsUrl={article.url}/>
                      </div>
-            })}
-          
+            })}          
         </div>
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark mx-1">Previous</button>
+        <button type="button" className="btn btn-dark mx-1" onClick={this.nextHandler}>Next</button>
       </div>
     );
   }
