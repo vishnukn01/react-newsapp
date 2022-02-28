e7348941c159f1500b473cb8492779ec785ed9d1
Vishnu KN <vishnunagaraj7@gmail.com>
2022-02-28 16:00:17 +0530

    News app initial setup

---
+++ src/components/NewsItem.js
@@ -0,0 +1,26 @@
+import { getByTitle } from "@testing-library/react";
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  
+  render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
+    let defaultImage = 'https://images.unsplash.com/photo-1495020689067-958852a7765e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80'
+    return (
+      <div className="my-3">
+        <div className="card" style={{ width: "18rem" }}>
+          <img className="card-img-top" src={imageUrl?imageUrl:defaultImage} alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
